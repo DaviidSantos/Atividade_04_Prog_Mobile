666feed4c49a63df2313aa4270420bd8cd640e00
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-09-29 18:14:00 -0300

    dashboard

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,42 +1,119 @@
-import { Avatar, Divider, ListItem } from '@rneui/base';
-import { ListItemContent } from '@rneui/base/dist/ListItem/ListItem.Content';
-import { ListItemSubtitle } from '@rneui/base/dist/ListItem/ListItem.Subtitle';
-import { ListItemTitle } from '@rneui/base/dist/ListItem/ListItem.Title';
 import React, { useEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
-import { axiosConfig } from '../lib/axios';
-import { Products } from '../types/products';
+import { View, Text, StyleSheet } from 'react-native';
+import axios from 'axios';
+import { BarChart } from 'react-native-chart-kit';
 
-const MainScreen = () => {
-    const [products, setProducts] = useState<Products[]>([])
+const Home = () => {
+  const [dashboard, setDashboard] = useState({
+    solicitacoes: 25,
+    solicitantes: 5,
+    itensDeAnalise: {
+      _sum: {
+        quantidadeDisponivel: 300,
+      },
+    },
+    ensaiosPendente: 2,
+    ensaiosEmAndamento: 10,
+    ensaiosConcluidos: 20,
+  });
 
-    useEffect(() => {
-        axiosConfig.get('products').then((resposta)=>{
-            setProducts(resposta.data.products)
-        })
-    }, [])
+  const data = {
+    labels: ['Pendente', 'Em Andamento', 'Concluídos'],
+    datasets: [
+      {
+        data: [dashboard.ensaiosPendente, dashboard.ensaiosEmAndamento, dashboard.ensaiosConcluidos],
+      },
+    ],
+  };
 
-    return (
-        <ScrollView>
-        {
-            products.map((product) => (
-                <ListItem key={product.id}>
-                        <Avatar source={{uri: product.thumbnail}} />
-                        <ListItemContent>
-                            <ListItemTitle>
-                                {product.title}
-                            </ListItemTitle>
-                            <ListItemSubtitle>
-                                ${product.price}
-                            </ListItemSubtitle>
-                        </ListItemContent>
-                    </ListItem>
-            ))
-        }
-        <Divider />
-      
-    </ScrollView>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Dashboard</Text>
+        <Text style={styles.subHeaderText}>Visão geral das informações do laboratório</Text>
+      </View>
+  
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>Total de Solicitações de Análise: {dashboard.solicitacoes}</Text>
+        <Text style={styles.infoText}>Total de solicitantes cadastrados: {dashboard.solicitantes}</Text>
+        <Text style={styles.infoText}>Total de itens de análise disponíveis: {dashboard.itensDeAnalise._sum.quantidadeDisponivel}</Text>
+      </View>
+  
+      <View style={styles.chartContainer}>
+        <Text style={styles.chartHeaderText}>Ensaios</Text>
+        <BarChart
+          data={data}
+          width={350}
+          height={200}
+          yAxisLabel=""
+          yAxisSuffix=""
+          chartConfig={{
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          fromZero={true} // Esta é a nova configuração
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+        <Text style={styles.chartStatusText}>Status dos ensaios no laboratório</Text>
+      </View>
+    </View>
+  );
 };
 
-export default MainScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#333',
+  },
+  subHeaderText: {
+    fontSize: 16,
+    color: '#555',
+  },
+  infoContainer: {
+    marginBottom: 20,
+  },
+  infoText: {
+    fontWeight: 'bold',
+    marginBottom: 8,
+    fontSize: 16,
+    color: '#333',
+  },
+  chartContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    padding: 20,
+    alignItems: 'center',
+  },
+  chartHeaderText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 8,
+    color: '#333',
+  },
+  chartStatusText: {
+    fontSize: 14,
+    color: '#555',
+  },
+});
+
+export default Home;
